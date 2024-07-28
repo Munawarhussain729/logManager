fb6690c2d33b13e5c2cdad6e140cdf5ae4b7d79b
Munawar <munawarhussa.7@gmail.com>
2024-07-28 22:54:29 +0500

    update project into javaScript project

--- src/app.js
+++ src/app.js
@@ -1,13 +1,17 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import bodyParser = require('body-parser');
-import { errorHandler, notFoundHandler } from './middlewares/errorHandlers';
+import bodyParser from 'body-parser';
+import { errorHandler, notFoundHandler } from './middlewares/errorHandlers.js';
 
 
 dotenv.config();
 
 const app = express()
 app.use(bodyParser.json())
+
+app.get('/',(req,res)=>{
+    return res.send('Hey')
+})
 app.use(notFoundHandler)
 app.use(errorHandler)
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import app from "./app"
+import app from "./app.js"
+
 const PORT = process.env.port || 3000
 
 app.listen(PORT,()=>{
