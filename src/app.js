2bd7da161918fd6033b85fec10debd8d3bb9fe10
Munawar <munawarhussa.7@gmail.com>
2024-07-28 23:06:14 +0500

    add dashboard view

--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@ import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser';
 import { errorHandler, notFoundHandler } from './middlewares/errorHandlers.js';
-
+import dashboardRouter from './routes/dashboard.js'
 
 dotenv.config();
 
 const app = express()
 app.use(bodyParser.json())
+app.set('view engine','ejs')
+app.set('views', './src/views');
+
+app.use('/dashboard', dashboardRouter)
 
 app.get('/',(req,res)=>{
     return res.send('Hey')
