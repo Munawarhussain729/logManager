65ac3e7dd6e258c9a850018fb4ac35e03afa21c2
Munawar <munawarhussa.7@gmail.com>
2024-08-03 00:55:13 +0500

    update dashboard design

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import { errorHandler, notFoundHandler } from './middlewares/errorHandlers.js';
-import dashboardRouter from './routes/dashboard.js'
+import dashboardRouter from './routes/dashboard.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
-const app = express()
-app.use(bodyParser.json())
-app.set('view engine','ejs')
+const app = express();
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 app.set('views', './src/views');
 
-app.use('/dashboard', dashboardRouter)
+// Get the __dirname equivalent in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.get('/',(req,res)=>{
-    return res.send('Hey')
-})
-app.use(notFoundHandler)
-app.use(errorHandler)
+// Serve static files
+app.use(express.static(path.join(__dirname, 'views')));
 
-export default app
+app.use('/dashboard', dashboardRouter);
+
+app.get('/', (req, res) => {
+    return res.send('Hey');
+});
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+export default app;
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -1,9 +1,9 @@
-import { Router } from "express";
+    import { Router } from "express";
 
-const router = Router()
+    const router = Router()
 
-router.get('/', (req, res) => {
-    res.render('dashboard', { title: 'Dashboard' })
-})
+    router.get('/', (req, res) => {
+        res.render('dashboard/dashboard', { title: 'Dashboard' })
+    })
 
-export default router
+    export default router
