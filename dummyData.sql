a89911cac53a62862cfd515b22b5617554884f2e
Munawar <munawarhussa.7@gmail.com>
2024-08-18 12:45:20 +0500

    add log insertion

--- dummyData.sql
+++ dummyData.sql
@@ -21,3 +21,17 @@ VALUES
 (gen_random_uuid(),'rizwan','rizwan@gmail.com',4,'rizwanPassword');
 
 
+INSERT INTO projects (name, client_name, project_manager_id)
+VALUES
+('Web rentify','Aleem',2),
+('Automate', 'jack',3);
+
+INSERT INTO user_projects(user_id, project_id)
+VALUES 
+(3,2),
+(2,1);
+
+INSERT INTO logs(message, blocker, user_role, duration, tomorrow_plan, project_id, user_id)
+VALUES
+('integrate add vheicle api', 'backend needs to update api according to requirement', 2,3, 'Will start working on inspect screen', 2,3),
+('Update the ticketing', 'No blocker.', 3,5, 'will be working on urgent tickets',1,2);
