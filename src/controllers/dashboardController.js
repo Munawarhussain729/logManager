0fcef4156a40804c8f2c78109e14f4174628ddc5
Munawar <munawarhussa.7@gmail.com>
2024-09-05 09:12:24 +0500

    update dashboard logs position

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -6,28 +6,22 @@ const { Pool } = pg
 const pool = new Pool(dbConfig)
 
 export const getDashboard = async (req, res) => {
+    let client = await pool.connect()
     try {
-        let client = await pool.connect()
-        const { date } = req.query;
-        console.log("Date is ", date);
+        const result = await client.query(`SELECT SUM (duration) FROM logs WHERE user_id = ${1}`)
+        
 
-        const result = await getTotalHours({ user_id: 1 })
-
-        if (!date) {
-            res.render('layouts/main', { title: 'Dashboard', duration: 12, contentFile: '../dashboard/dashboard' })
-            return
-        }
-
-        if (!result) {
+        // const result = await getTotalHours({ user_id: 1 })
+        if (!result || result.rows.length <=0) {
             res.render('layouts/main', { title: 'Dashboard', duration: 0, contentFile: '../dashboard/dashboard' });
             return
         }
-
+        const totalHours = result.rows[0]
         res.render('layouts/main', { title: 'Dashboard', duration: result?.sum, contentFile: '../dashboard/dashboard' });
-
     } catch (error) {
         console.error('Error is ', error)
         res.status(500).send('Internal Server error')
+    }finally{
+        client.release()
     }
-
 };
--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -4,31 +4,6 @@ import { getDailyLogs, postDailyLog } from "../controllers/dailylogsController.j
 
 const router = Router();
 
-const tempLogData = [
-    {
-        created_on: '2024-08-18T07:10:35.984Z',
-        message: 'Update the ticketing',
-        blocker: 'No blocker.',
-        duration: 5,
-        tomorrow_plan: 'will be working on urgent tickets',
-        id: 4,
-        project_id: 1,
-        user_id: 2,
-        user_role: 3
-    },
-    {
-        created_on: '2024-08-18T07:10:35.984Z',
-        message: 'integrate add vheicle api',
-        blocker: 'backend needs to update api according to requirement',
-        duration: 3,
-        tomorrow_plan: 'Will start working on inspect screen',
-        id: 3,
-        project_id: 2,
-        user_id: 3,
-        user_role: 2
-      }
-]
-
 router.get('/',getDashboard);
 
 export default router;
