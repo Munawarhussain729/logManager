d69a36fe854c3fd83b63c33c6400c59462d96230
Munawar <munawarhussa.7@gmail.com>
2024-09-12 08:10:34 +0500

    fetch current day hours for dashboard

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,27 +1,34 @@
 import dbConfig from "../dbconfig.js";
 import { getTotalHours } from "../utils/helperFunction.js";
-import pg from 'pg'
+import pg from 'pg';
 
-const { Pool } = pg
-const pool = new Pool(dbConfig)
+const { Pool } = pg;
+const pool = new Pool(dbConfig);
 
 export const getDashboard = async (req, res) => {
-    let client = await pool.connect()
+    let client = await pool.connect();
     try {
-        const result = await client.query(`SELECT SUM (duration) FROM logs WHERE user_id = ${1}`)
-        
+        const date = new Date();
+        const formattedDate = date.toISOString().split('T')[0];  // Correct date format
 
-        // const result = await getTotalHours({ user_id: 1 })
-        if (!result || result.rows.length <=0) {
-            res.render('layouts/main', { title: 'Dashboard', duration: 0, contentFile: '../dashboard/dashboard' });
-            return
+        const result = await client.query(`SELECT SUM(duration) FROM logs WHERE user_id = $1`, [1]);
+        const currentDayHours = await client.query(`SELECT SUM(duration) FROM logs WHERE user_id = $1 AND created_on = $2`, [1, formattedDate]);
+        console.log("Current day houts ", currentDayHours.rows)
+        let todayHours = 0
+        if(currentDayHours && currentDayHours.rows.length > 0){
+            todayHours = currentDayHours.rows[0].sum
         }
-        const totalHours = result.rows[0]
-        res.render('layouts/main', { title: 'Dashboard', duration: result?.sum, contentFile: '../dashboard/dashboard' });
+        if (!result || result.rows.length <= 0) {
+            res.render('layouts/main', { title: 'Dashboard', duration: 0, contentFile: '../dashboard/dashboard' });
+            return;
+        }
+
+        const totalHours = result.rows[0].sum || 0;
+        res.render('layouts/main', { title: 'Dashboard', duration: totalHours, currentDayHours: todayHours, contentFile: '../dashboard/dashboard' });
     } catch (error) {
-        console.error('Error is ', error)
-        res.status(500).send('Internal Server error')
-    }finally{
-        client.release()
+        console.error('Error is ', error);
+        res.status(500).send('Internal Server error');
+    } finally {
+        client.release();
     }
 };
