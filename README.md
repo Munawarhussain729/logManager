97f862f4afc9feaa87e15f89333481b89f02ca00
Munawar Hussain <munawarhussa.7@gmail.com>
2024-12-28 21:56:58 +0500

    add read me instructions to initiate project

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # logManager
+
+Start your postgress service
+sudo service postgresql start
+
+Check it's status:
+sudo service postgresql status
+
+#### Login into postgress 
+sudo -i -u postgres
+psql
+
+#### Create user and database for the project
+CREATE USER log_manager WITH PASSWORD "log_manager"
---
+++ knexfile.js
@@ -0,0 +1,55 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'postgres',
+      database: 'log_manager',
+    },
+    migrations: {
+      directory: './src/migrations',
+    },
+    seeds: {
+      directory: './src/seeds',
+    },
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'logmanager',
+      user: 'postgres',
+      password: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'logmanager',
+      user: 'postgres',
+      password: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
